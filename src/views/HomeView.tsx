9e23a7b8fd7df6a725f48c101c544e508a836d45
fgamester <falvarez.munoz@hotmail.es>
2025-02-21 20:45:05 -0300

    responsive started

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,21 +1,21 @@
-import Exercises from "../components/home/Exercises"
-import KnownTechnologies from "../components/home/KnownTechnologies"
-import Projects from "../components/home/Projects"
-import SideNavBar from "../components/SideNavBar"
-import data from '../../test_data/data_example.json'
+import Exercises from "../components/home/Exercises";
+import KnownTechnologies from "../components/home/KnownTechnologies";
+import Projects from "../components/home/Projects";
+// import SideNavBar from "../components/SideNavBar";
+import data from '../../test_data/data_example.json';
 
 export default function HomeView() {
   return (
     <div className='flex flex-col items-center gap-pf-4 pt-pf-4 text-pf-dark-1'>
       <header className=''>
-        <h1 className="text-4xl">
+        <h1 className="text-4xl text-center">
           Welcome to FGamester's Portfolio
         </h1>
       </header>
-      <div className='flex gap-7 sm:px-pf-4'>
+      <div className='flex gap-7 w-full px-pf-2 sm:px-pf-4'>
         {/*Side Navigation Bar*/}
-        <SideNavBar />
-        <main className='w-3/4'>
+        {/* <SideNavBar /> */}
+        <main className='md:w-3/4'>
           {/*Featured*/}
           <section id='featured' className="flex flex-col gap-pf-3">
             {/*Technologies*/}
