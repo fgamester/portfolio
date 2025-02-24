28fc26c442465b3406711a7d07a4dcea8cca4216
fgamester <falvarez.munoz@hotmail.es>
2025-02-24 19:56:43 -0300

    general content preview updated

--- src/components/global/ContentPreview.tsx
+++ src/components/global/ContentPreview.tsx
@@ -16,9 +16,9 @@ export default function ContentPreview({ item, category, index = 0 }: { item: Pr
       <div>
         <Badges badgeList={item.tags} />
       </div>
-      <div className={`flex ${index % 2 !== 0 && 'flex-row-reverse'} gap-pf-2 w-full`}>
-        <p className="text-justify text-base w-1/2">{item.description}</p>
-        <div className="flex justify-center items-center aspect-video overflow-hidden bg-pf-dark-2 rounded-xl w-1/2">
+      <div className={`flex items-center flex-col-reverse ${index % 2 !== 0 ? 'md:flex-row-reverse':'md:flex-row'} md:items-start gap-pf-2 w-full`}>
+        <p className="text-justify text-base md:w-1/2">{item.description}</p>
+        <div className="flex justify-center items-center aspect-video overflow-hidden bg-pf-dark-2 rounded-xl sm:w-3/4 md:w-1/2">
           {
             item.image ?
               <img className="object-cover w-full h-full" src={item.image} alt={`${item.name}_image`} />
