9b2a566695be648f3265fde516614ce1533c48ac
fgamester <falvarez.munoz@hotmail.es>
2025-03-05 00:30:21 -0300

    basic info and links sections added

--- src/views/ContentView.tsx
+++ src/views/ContentView.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../context/GloblalContext";
 import { useParams } from "react-router-dom";
-import { Content, Data, Project } from "../types";
+import { Content, Data, isProject, Project } from "../types";
 
 export default function ContentView() {
     const [post, setPost] = useState<Project | undefined>(undefined);
@@ -18,16 +18,43 @@ export default function ContentView() {
         }
     }, [data, params])
 
-    return (
-        <div className="bg-pf-dark-3">
-            <div className="flex flex-col">
-                <p>
-                    {params.category}
-                </p>
-                <p>
-                    {post?.id}
-                </p>
-            </div>
+    return isProject(post) ? (
+        <div className="bg-pf-dark-4 md:bg-transparent md:p-pf-4 w-full flex flex-col justify-center gap-pf-3 text-pf-dark-1">
+            <header className="flex justify-center">
+                <h1 className="text-4xl">
+                    {post?.name}
+                </h1>
+            </header>
+            <main className="flex flex-col gap-pf-4">
+                <section id="info" className="w-full flex flex-col md:bg-pf-dark-4 md:p-pf-3 md:rounded-2xl lg:flex-row">
+                    <p>
+                        {post?.description}
+                    </p>
+                    {
+                        post.image && (
+                            <img src={post.image} alt={`${post.name}_image`} className="lg:w-1/2" />
+                        )
+                    }
+                </section>
+                {
+                    post.links && (
+                        <section id="links" className="w-full flex flex-col justify-center items-center gap-pf-2  md:bg-pf-dark-4 md:p-pf-3 md:rounded-2xl">
+                            <h4 className="text-xl">
+                                Links
+                            </h4>
+                            <div className="flex flex-wrap justify-center gap-pf-2">
+                                {
+                                    post.links.map((item, index) => (
+                                        <a href={item.link} key={index} target="_blank" className="py-pf-1 px-pf-2 rounded-2xl bg-pf-dark-1 text-pf-dark-6">
+                                            {item.name}
+                                        </a>
+                                    ))
+                                }
+                            </div>
+                        </section>
+                    )
+                }
+            </main>
         </div>
-    )
+    ) : null;
 }
