b6144d0c3a9a464f6fb3d3311254b1d565f678f5
fgamester <falvarez.munoz@hotmail.es>
2025-03-06 17:06:28 -0300

    basic guard functions for all types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,9 +3,35 @@ export type ProjectTechnology = {
     usedFor: string
 }
 
+export function isProjectTechnology(item: any): item is ProjectTechnology {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'usedFor'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop))
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
+}
+
 export type Technology = {
     name: string,
     image: string
+}
+
+export function isTechnology(item: any): item is Technology {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'image'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop))
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
 export type ProjectLink = {
@@ -14,10 +40,38 @@ export type ProjectLink = {
     icon?: string
 }
 
+export function isProjectLink(item: any): item is ProjectLink {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'link'];
+    const optionalProperties = ['icon'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
+}
+
 export type GuideSteps = {
     name: string,
     instructions: string,
     image?: string
+}
+
+export function isGuideSteps(item: any): item is GuideSteps {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'instructions'];
+    const optionalProperties = ['image'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
 export type ProjectGuide = {
@@ -25,6 +79,20 @@ export type ProjectGuide = {
     description: string,
     videoLink?: string,
     steps: GuideSteps[]
+}
+
+export function isProjectGuide(item: any): item is ProjectGuide {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'description', 'steps'];
+    const optionalProperties = ['videoLink'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
 export type Project = {
@@ -39,10 +107,38 @@ export type Project = {
     guides?: ProjectGuide[]
 }
 
+export function isProject(item: any): item is Project {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['id', 'name', 'date', 'description', 'tags'];
+    const optionalProperties = ['image', 'technologies', 'links', 'guides'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
+}
+
 export type Content = {
     description: string,
     info?: string,
     content: Project[]
+}
+
+export function isContent(item: any): item is Content {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['description', 'content'];
+    const optionalProperties = ['info'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
 export type Activity = {
@@ -51,10 +147,38 @@ export type Activity = {
     image?: string
 }
 
+export function isActivity(item: any): item is Activity {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'description'];
+    const optionalProperties = ['image'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
+}
+
 export type Hobby = {
     name: string,
     description: string,
     image?: string
+}
+
+export function isHobby(item: any): item is Hobby {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'description'];
+    const optionalProperties = ['image'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
 export type Data = {
@@ -70,10 +194,20 @@ export type Data = {
     hobbies?: (Hobby | Activity)[]
 }
 
-export type ContextProps = {
-    data: Data | undefined
+export function isData(item: any): item is Data {
+    if (!item || Array.isArray(item) || typeof item !== 'object') {
+        return false;
+    }
+    const requiredProperties = ['name', 'alias'];
+    const optionalProperties = ['about', 'technologies', 'projects', 'exercises', 'hobbies'];
+    const itemKeys = Object.keys(item);
+
+    const hasRequiredProperties = requiredProperties.every(prop => prop in item);
+    const hasOnlyAllowedProperties = itemKeys.every(prop => requiredProperties.includes(prop) || optionalProperties.includes(prop));
+
+    return hasRequiredProperties && hasOnlyAllowedProperties;
 }
 
-export function isProject(item: any): item is Project {
-    return item && typeof item === 'object' && 'id' in item && 'name' in item && 'date' in item && 'description' in item && Array.isArray(item.tags) && Array.isArray(item.technologies);
+export type ContextProps = {
+    data: Data | undefined
 }
