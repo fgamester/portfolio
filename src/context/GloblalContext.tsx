b7675b51aa1ee944f24ba72b6d7b6b0573f9a58c
fgamester <falvarez.munoz@hotmail.es>
2025-03-19 16:28:03 -0300

    data structure updated

--- src/context/GloblalContext.tsx
+++ src/context/GloblalContext.tsx
@@ -3,20 +3,23 @@ import { Data, ContextProps, formatData } from "../types";
 
 export const Context = createContext<ContextProps>({
     data: undefined,
-    updateState: undefined
+    updateState: undefined,
+    globalLoading: true
 });
 
 export const GlobalContext = ({ children }: { children: ReactNode }) => {
     const [data, setData] = useState<Data | undefined>(undefined);
+    const [globalLoading, setGlobalLoading] = useState<boolean>(true);
     const updateState = useCallback((newState: any, setState: (state: any) => void) => setState(() => newState), []);
 
     const fetchData = useCallback(async () => {
-        const dataUrl = import.meta.env.VITE_DATA_URL;
-        const dataExampleUrl = import.meta.env.VITE_DATA_EXAMPLE_URL;
-        let newData: Data | undefined;
+        const fetchUrl = {
+            data: import.meta.env.VITE_DATA_URL,
+            example: import.meta.env.VITE_DATA_EXAMPLE_URL
+        };
         try {
-            const resp = await fetch(dataExampleUrl);
-            newData = await resp.json();
+            const resp = await fetch(fetchUrl.example);
+            const newData = await resp.json();
             updateState(formatData(newData), setData);
         } catch (error) {
             console.error(error);
@@ -29,7 +32,7 @@ export const GlobalContext = ({ children }: { children: ReactNode }) => {
     }, []);
 
     return (
-        <Context.Provider value={{ data, updateState }}>
+        <Context.Provider value={{ data, updateState, globalLoading }}>
             {children}
         </Context.Provider>
     );
