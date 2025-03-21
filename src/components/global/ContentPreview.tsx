59f595da7dba63be113a1d531710c851072c333f
fgamester <falvarez.munoz@hotmail.es>
2025-03-21 14:54:34 -0300

    styles fixed

--- src/components/global/ContentPreview.tsx
+++ src/components/global/ContentPreview.tsx
@@ -5,7 +5,7 @@ import Badges from "./Badges";
 export default function ContentPreview({ item, group, index = 0 }: { item: ProjectType, group: 'projects' | 'exercises', index?: number }) {
   return (
     <article className='flex justify-evenly flex-wrap p-pf-4 gap-pf-2 bg-pf-dark-4 rounded-2xl'>
-     <header className="w-full flex justify-between items-start">
+      <header className="w-full flex justify-between items-start">
         <Link to={`/content/${group}/${item.id}`} className="text-xl text-start">
           {item.name}
         </Link>
@@ -17,15 +17,14 @@ export default function ContentPreview({ item, group, index = 0 }: { item: Proje
       <div className={`flex items-center flex-col-reverse ${index % 2 !== 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:items-start gap-pf-2 w-full`}>
         <p className="text-justify text-base lg:w-1/2">{item.description}</p>
         <div className="flex justify-center items-center aspect-video overflow-hidden bg-pf-dark-2 rounded-xl md:w-3/4 lg:w-1/2">
-          {
-            item.image ?
-              <img className="object-cover w-full h-full" src={item.image} alt={`${item.name}_image`} />
-              :
-              <div className="flex flex-col justify-center items-center">
-                <img className="" src='default_image.svg' alt='default_image' />
-                <p className="text-black text-xl text-center">Image Not Found</p>
-              </div>
-          }
+          {item.image ? (
+            <img className="object-cover w-full h-full" src={item.image} alt={`${item.name}_image`} />
+          ) : (
+            <div className="flex flex-col justify-center items-center">
+              <img className="" src='default_image.svg' alt='default_image' />
+              <p className="text-black text-xl text-center">Image Not Found</p>
+            </div>
+          )}
         </div>
       </div>
     </article>
@@ -47,15 +46,14 @@ export function HomeContentPreview({ item, group, index = 0 }: { item: ProjectTy
       <div className={`flex items-center flex-col-reverse ${index % 2 !== 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:items-start gap-pf-2 w-full`}>
         <p className="text-justify text-base lg:w-1/2">{item.description}</p>
         <div className="flex justify-center items-center aspect-video overflow-hidden bg-pf-dark-2 rounded-xl md:w-3/4 lg:w-1/2">
-          {
-            item.image ?
-              <img className="object-cover w-full h-full" src={item.image} alt={`${item.name}_image`} />
-              :
-              <div className="flex flex-col justify-center items-center">
-                <img className="" src='default_image.svg' alt='default_image' />
-                <p className="text-black text-xl text-center">Image Not Found</p>
-              </div>
-          }
+          {item.image ? (
+            <img className="object-cover w-full h-full" src={item.image} alt={`${item.name}_image`} />
+          ) : (
+            <div className="flex flex-col justify-center items-center">
+              <img className="" src='default_image.svg' alt='default_image' />
+              <p className="text-black text-xl text-center">Image Not Found</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
