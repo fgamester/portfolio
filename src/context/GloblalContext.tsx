f9f1679bd797200ca3c2f495cf8f06c9c7680250
fgamester <falvarez.munoz@hotmail.es>
2025-03-24 16:35:19 -0300

    types updated, global context improved

--- src/context/GloblalContext.tsx
+++ src/context/GloblalContext.tsx
@@ -1,16 +1,17 @@
 import { createContext, useState, ReactNode, useEffect, useCallback } from "react";
 import { Data, ContextProps, formatData } from "../types";
 
+const updateState = (newState: any, setState: (state: any) => void) => setState(() => newState);
+
 export const Context = createContext<ContextProps>({
     data: undefined,
-    updateState: undefined,
+    updateState,
     globalLoading: true
 });
 
 export const GlobalContext = ({ children }: { children: ReactNode }) => {
     const [data, setData] = useState<Data | undefined>(undefined);
     const [globalLoading, setGlobalLoading] = useState<boolean>(true);
-    const updateState = useCallback((newState: any, setState: (state: any) => void) => setState(() => newState), []);
 
     const fetchData = useCallback(async () => {
         const fetchUrl = {
